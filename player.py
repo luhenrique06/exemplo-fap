b66cefdd6f2ac9b3bd23333fb2838d19bca2d725
Luiz Henrique Custódio Mendes Marques <luiz@MacBook-Air-de-Luiz.local>
2025-10-15 15:33:47 -0300

    Auula 2

---
+++ player.py
@@ -0,0 +1,49 @@
+
+import pygame
+from settings import *
+
+
+
+class Player(pygame.sprite.Sprite):
+	"""Classe que representa o jogador controlável"""
+
+	def __init__(self, pos, groups):
+		
+		super().__init__(groups)
+
+		self.image = pygame.Surface((64, 64))  
+		self.image.fill('blue')  
+
+		self.rect = self.image.get_rect(center=pos)
+
+		self.pos = pygame.math.Vector2(self.rect.center)
+		self.direction = pygame.math.Vector2(0, 0) 
+		self.speed = 200
+
+
+	def input(self):
+		"""Captura as teclas pressionadas e define a direção do movimento"""
+		keys = pygame.key.get_pressed()
+
+
+		self.direction.x = 0
+		self.direction.y = 0
+
+	
+
+	def move(self, dt):
+		"""Move o personagem baseado na direção e velocidade"""
+
+		self.pos.x += self.direction.x * self.speed * dt
+		self.pos.y += self.direction.y * self.speed * dt
+
+		self.rect.centerx = round(self.pos.x)
+		self.rect.centery = round(self.pos.y)
+
+
+	def update(self, dt):
+		"""Método chamado a cada frame"""
+		self.input()    
+		self.move(dt)     
+
+
